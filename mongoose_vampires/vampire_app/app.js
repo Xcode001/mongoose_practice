ca692f647d8468af57caefae907c9fdef14c06e4
myDeveloperJourney <daniel@mydeveloperjourney.com>
2018-10-12 10:32:51 -0500

    adds basic queries

--- mongoose_vampires/vampire_app/app.js
+++ mongoose_vampires/vampire_app/app.js
@@ -15,17 +15,48 @@ mongoose.connect(connectionString, {useNewUrlParser: true})
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
-Vampire.insertMany(vampires, (err, response) => {
-    if(err) console.log('An Error was thrown: ', err)
-    else console.log(response)
-})
+// Vampire.insertMany(vampires, (err, response) => {
+//     if(err) console.log('An Error was thrown: ', err)
+//     else console.log(response)
+// })
 // ### Add some new vampire data
+
+// Vampire.create({
+//     name: "Daniel",
+//     hair_color: "blonde",
+//     eye_color: "Green",
+//     dob: "Jan 30, 1985",
+//     loves: ['coding', 'friday morning tacos'],
+//     location: "Dallas, TX",
+//     gender: 'm',
+//     victims: 1
+
+// }, (error, response) => {
+//     if(error) console.log(error)
+//     else console.log(response)
+// })
 
 /////////////////////////////////////////////////
 // ## QUERYING
 /////////////////////////////////////////////////
 // ### Select by comparison
+// Vampire.find({gender: 'f'}, (error, response) => {
+//     if(error) console.log(error)
+//     else console.log(response)
+// })
+// Vampire.find({
+//     victims: {$gt: 500}
+// }, (error, response) => {
+//     if(error) console.log(error)
+//     else console.log(response)
+// })
 
+Vampire.find({
+    victims: {$ne: 210234}
+}, (error, response) => {
+    if(error) console.log(error)
+    else console.log(response)
+})
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
 
