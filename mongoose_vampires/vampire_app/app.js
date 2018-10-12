e423a68dbc2056780c4bad70af086f5267456902
myDeveloperJourney <daniel@mydeveloperjourney.com>
2018-10-12 09:57:49 -0500

    schema tests pass

--- mongoose_vampires/vampire_app/app.js
+++ mongoose_vampires/vampire_app/app.js
@@ -1,5 +1,5 @@
 // 1. Require your node modules
-
+const mongoose = require('mongoose')
 // 2. Require your model (and possibly your extra data source);
 
 // 3. Connect your database and collection name
