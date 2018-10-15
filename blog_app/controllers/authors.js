91bfdbdaa597837906c9e19ef23a76c7d789c308
myDeveloperJourney <daniel@mydeveloperjourney.com>
2018-10-15 11:42:57 -0500

    further builds out authors

--- blog_app/controllers/authors.js
+++ blog_app/controllers/authors.js
@@ -1,8 +1,27 @@
 const authorsController = require('express').Router()
+const Author = require('../models/author')
 
-
-authorsController.get('/', (req, res) => {
-    res.render('authors/index.ejs')
+authorsController.get('/', async (req, res) => {
+    try {
+        const allAuthors = await Author.find({})
+        res.render('authors/index.ejs', {allAuthors})
+    } catch (error) {
+        console.log(error)
+        res.send("Something Went Wrong")
+    }
 })
 
+authorsController.post('/', async (req, res) => {
+    try {
+        await Author.create(req.body)
+        res.redirect('/authors')
+    } catch (error) {
+        console.log("Error", error)
+        res.send("Something Went Wrong")
+    }
+})
+
+authorsController.get('/new', (req, res) => {
+    res.render('authors/new.ejs')
+})
 module.exports = authorsController
