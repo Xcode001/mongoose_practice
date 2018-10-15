8bb4981c3715df7f4977c7bf59132ba48ab52915
myDeveloperJourney <daniel@mydeveloperjourney.com>
2018-10-15 12:46:00 -0500

    adds updates to the authors / blogs application

--- blog_app/controllers/authors.js
+++ blog_app/controllers/authors.js
@@ -6,8 +6,8 @@ authorsController.get('/', async (req, res) => {
         const allAuthors = await Author.find({})
         res.render('authors/index.ejs', {allAuthors})
     } catch (error) {
-        console.log(error)
-        res.send("Something Went Wrong")
+        console.log("Error", error)
+        res.status(404).send("Something Went Wrong - Author Not Found")
     }
 })
 
@@ -17,11 +17,51 @@ authorsController.post('/', async (req, res) => {
         res.redirect('/authors')
     } catch (error) {
         console.log("Error", error)
-        res.send("Something Went Wrong")
+        res.status(500).send("Something Went Wrong")
     }
 })
 
 authorsController.get('/new', (req, res) => {
     res.render('authors/new.ejs')
+})
+
+authorsController.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit.ejs', {author})
+    } catch (error) {
+        console.log("Error", error)
+        res.status(500).send("Something Went Wrong")
+    }
+})
+
+authorsController.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/show.ejs', {author})
+    } catch (error) {
+        console.log("Error", error)
+        res.status(404).send("Something Went Wrong, Author Not Found")
+    }
+})
+
+authorsController.put('/:id', async (req, res) => {
+    try {
+        await Author.findByIdAndUpdate(req.params.id, req.body)
+        res.redirect('/authors/' + req.params.id)
+    } catch (error) {
+        console.log("Error", error)
+        res.status(500).send("Something Went Wrong")
+    }
+})
+
+authorsController.delete('/:id', async (req, res) => {
+    try {
+        await Author.findByIdAndDelete(req.params.id)
+        res.redirect('/authors')
+    } catch (error) {
+        console.log("Error", error)
+        res.status(500).send("Something Went Wrong")
+    }
 })
 module.exports = authorsController
