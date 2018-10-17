e9d64b646c423a17999276009dd93c3291b45ebb
myDeveloperJourney <daniel@mydeveloperjourney.com>
2018-10-16 21:02:17 -0500

    adds latest updates

--- blog_app/controllers/blogs.js
+++ blog_app/controllers/blogs.js
@@ -12,16 +12,29 @@ blogsRouter.get('/', async (req, res) => {
     }
 })
 
-blogsRouter.post('/', (req, res) => {
-    Author.findById(req.body.authorId, (err, author) => {
-        Blog.create(req.body, (err, blog) => {
-            author.blogs.push(blog)
-            author.save((err, response) => {
-                console.log(response)
-                res.redirect('/blogs')
-            })
-        })
+// blogsRouter.post('/', (req, res) => {
+//     Author.findById(req.body.authorId, (err, author) => {
+//         Blog.create(req.body, (err, blog) => {
+//             author.blogs.push(blog)
+//             author.save((err, response) => {
+//                 res.redirect('/blogs')
+//             })
+//         })
+//     })
+// })
+
+blogsRouter.post('/', async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body)
+        await Author.findByIdAndUpdate({
+            "_id": blog.authorId
+        },{$push: {blogs: blog}
     })
+        res.redirect('/blogs')
+    } catch (error) {
+        console.log(error)
+        res.send('error')
+    }
 })
 
 
@@ -36,24 +49,38 @@ blogsRouter.get('/new', async (req, res) => {
 })
 
 
-blogsRouter.get('/:id', (req, res) => {
-    Blog.findById(req.params.id, (err, blog) => {
-        Author.findOne({'blogs._id': req.params.id}, (err, author) => {
-            res.render('blogs/show.ejs', {
-                blog,
-                author
-            })
-        })
-    })
+// blogsRouter.get('/:id', (req, res) => {
+//     Blog.findById(req.params.id, (err, blog) => {
+//         Author.findOne({'blogs._id': req.params.id}, (err, author) => {
+//             res.render('blogs/show.ejs', {
+//                 blog,
+//                 author
+//             })
+//         })
+//     })
+// })
+
+blogsRouter.get('/:id', async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id)
+        const author = await Author.findById(blog.authorId)
+        res.render('blogs/show.ejs', {blog, author})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Somthing went wrong, check your console")
+    }
 })
 
 blogsRouter.delete('/:id', async (req, res) => {
     try {
-        await Blog.findByIdAndDelete(req.params.id)
+        const blogToDelete = await Blog.findByIdAndDelete(req.params.id)
+        await Author.findByIdAndUpdate(blogToDelete.authorId, {
+            $pull : { blogs : { _id: req.params.id}}
+        })
         res.redirect('/blogs')
-    } catch (error) {
-        console.log("Error", error)
-        res.status(500).send("Something Went Wrong")
+    } catch(error){
+       console.log(error)
+       res.status(500).send('Something went wrong, check your console')
     }
 })
 
@@ -67,10 +94,36 @@ blogsRouter.get('/:id/edit', async (req, res) => {
     }
 })
 
+// blogsRouter.put('/:id', async (req, res) => {
+//     try {
+//         await Blog.findByIdAndUpdate(req.params.id, req.body, 
+//             {new: true}, (err, updatedBlog) => {
+//             Author.findOne({'blogs._id': req.params.id}, (err, author) => {
+//                 author.blogs.id(req.params.id).remove()
+//                 author.blogs.push(updatedBlog)
+//                 author.save((error, response) => {
+//                     res.redirect('/blogs/' + req.params.id)
+//                 })
+//             })
+//         })
+//     } catch (error) {
+//         console.log("Error", error)
+//         res.status(500).send("Something Went Wrong")
+//     }
+// })
+
 blogsRouter.put('/:id', async (req, res) => {
     try {
-        await Blog.findByIdAndUpdate(req.params.id, req.body)
-        res.redirect('/blogs/'+req.params.id)
+        const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true})
+        await Author.findOneAndUpdate({
+            "_id": blog.authorId,
+            "blogs._id": req.params.id
+        },{
+            "$set":{
+                "blogs.$": req.body
+            }
+        })
+        res.redirect('/blogs/' + req.params.id)
     } catch (error) {
         console.log("Error", error)
         res.status(500).send("Something Went Wrong")
@@ -78,3 +131,4 @@ blogsRouter.put('/:id', async (req, res) => {
 })
 
 module.exports = blogsRouter
+
